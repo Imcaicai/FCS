f15fc0226b8a69dcddf6a6869171ab747124aafb
zhenya <2928664165@qq.com>
2023-03-26 19:22:43 +0800

    evaluate model

--- bert/predict.py
+++ bert/predict.py
@@ -21,14 +21,16 @@ def create_examples(lines, set_type):
     for (i, line) in enumerate(lines):
         guid = "%s-%s" % (set_type, i)
         # label = int(line[1])
-        text_a = line[1]
+        # CNM!!@!!
+        text_a = line[2].replace("YZYHUST", ',')
         examples.append(
             InputExample(guid=guid, text_a=text_a, text_b=None, label=None))
     return examples
 
 
 def Load_data(args, tokenizer):
-    with open(os.path.join(args.data_dir, 'predict.csv'), 'r') as f:
+    csv.field_size_limit(500 * 1024 * 1024)
+    with open(os.path.join(args.data_dir, 'val.csv'), 'r') as f:
         examples = create_examples(list(csv.reader(f)), 'predict')
     label_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     features = features = convert_examples_to_features(
@@ -53,17 +55,17 @@ def Load_data(args, tokenizer):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_dir",
-                        default="./url",
+                        default="./url/predict",
                         type=str,
                         required=False,
                         help="Locate the dataset")
     parser.add_argument("--model_dir",
-                        default='./outs',
+                        default='./model',
                         type=str,
                         required=False,
                         help="Locate the pre-trained model")
     parser.add_argument("--max_seq_length",
-                        default=128,
+                        default=256,
                         type=int,
                         required=False)
     args = parser.parse_args()
@@ -100,11 +102,16 @@ def main():
             preds = logits.detach().cpu().numpy()
         else:
             preds = np.append(preds, logits.detach().cpu().numpy(), axis=0)
-
-    preds = np.argmax(preds, axis=1)
-    preds = pd.DataFrame(preds)
+    m = torch.nn.Softmax(dim=1)
+    prob = np.array(m(torch.Tensor(preds)))
+    pred = np.argmax(preds, axis=1)
+    prob = np.max(prob, axis=1)
+    pred = pd.DataFrame(pred)
+    prob = pd.DataFrame(prob)
+    prediction = pd.concat([pred, prob], axis=1)
+    prediction.columns = ['pred', 'prob']
     # save results
-    preds.to_csv(header=None, index=None, path_or_buf='./predict.csv')
+    prediction.to_csv(index=None, path_or_buf='./predict.csv')
 
 
 if __name__ == "__main__":
