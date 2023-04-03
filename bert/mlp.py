86d42bb4d1a90955ee9a6382dc4f24918d259e7b
zhenya <2928664165@qq.com>
2023-04-03 22:12:11 +0800

    add features

--- bert/mlp.py
+++ bert/mlp.py
@@ -4,10 +4,10 @@ import torch.nn as nn
 
 
 class mlp(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, in_features=768, out_features=13) -> None:
         super().__init__()
         self.net=nn.Sequential(
-            nn.Linear(in_features=768,out_features=1024),\
+            nn.Linear(in_features=in_features,out_features=1024),\
             nn.ReLU(),
             nn.Dropout(p=0.5),
             nn.Linear(in_features=1024,out_features=512),\
@@ -19,7 +19,7 @@ class mlp(nn.Module):
             nn.Linear(in_features=512,out_features=256),
             nn.ReLU(),
             nn.Dropout(p=0.2),
-            nn.Linear(in_features=256,out_features=13)
+            nn.Linear(in_features=256,out_features=out_features)
         )
 
     def forward(self, x):
